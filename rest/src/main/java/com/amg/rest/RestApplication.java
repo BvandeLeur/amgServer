74743b4718ee353ea4f8c5959c932a68908cdafc
Bart <bart.1997@hotmail.com>
2019-06-17 21:01:55 +0200

    Lombok , websockets.

--- rest/src/main/java/com/amg/rest/RestApplication.java
+++ rest/src/main/java/com/amg/rest/RestApplication.java
@@ -2,13 +2,23 @@ package com.amg.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
+
+
+@EntityScan(basePackages = {"com"})
+@ServletComponentScan
 
 @SpringBootApplication
 @ComponentScan("com")
 public class RestApplication {
 
+
     public static void main(String[] args) {
+
+
+
         SpringApplication.run(RestApplication.class, args);
     }
 

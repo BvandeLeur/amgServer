e23b6aefe58736b939e012c64b8183da778041b0
Bart <bart.1997@hotmail.com>
2019-06-17 19:35:12 +0200

    Lombok , websockets.

---
+++ domain/src/main/java/com/amg/models/GreetingModel.java
@@ -0,0 +1,18 @@
+package com.amg.models;
+
+public class GreetingModel {
+
+    private String content;
+
+    public GreetingModel() {
+    }
+
+    public GreetingModel(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
---
+++ domain/src/main/java/com/amg/models/socketMessage.java
@@ -0,0 +1,21 @@
+package com.amg.models;
+
+public class socketMessage {
+
+    private String name;
+
+    public socketMessage() {
+    }
+
+    public socketMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
